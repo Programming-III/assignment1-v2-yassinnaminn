fc0923f06142c5f5982104fd9f590645b65d22ec
yassinnaminn <yassinahmedamin06@gmail.com>
2025-11-11 09:52:34 +0200

    complete main.cpp

--- main.cpp
+++ main.cpp
@@ -10,39 +10,94 @@ using namespace std;
 
 // ==================== Person Class Implementation =========================
 
+Person(){
+	std::string name = "";
+	int id = 0;
+}
+Person(std::string name, int id) {
+	this->name = name;
+	this->id = id;
+}
+~Person() {
 
-
-
-
-
-
+}
+void display() {
+	cout << name << endl;
+	cout << id << endl;
+}
 
 // ==================== Student Class Implementation ====================
+Student() {
 
+}
+Student(int yearLevel, std::string major) {
+	this->yearLevel = yearLevel;
+	this->major = major;
+}
+~Student() {
 
-
-
-
+}
+void display() {
+	cout << yearLevel << endl;
+	cout << major << endl;
+}
 
 // ==================== Instructor Class Implementation ====================
+Instructor() {
 
+}
+Instructor(std::string name, int experienceYears) {
+	this->name = name;
+	this->experienceYears = experienceYears;
+}
+~Instructor() {
 
-
-
-
+}
+void display() {
+	cout << name << endl;
+	cout << experienceYears << endl;
+}
 
 // ==================== Course Class Implementation ====================
 
+Course() {
 
+}
+Course(std::string courseCode, std::string courseName, int maxStudents, Student* student, int currentStudents) {
+	this->courseCode = courseCode;
+	this->courseName = courseName;
+	this->maxStudents = maxStudents;
+	this->student = student;
+	this->currentStudents = currentStudents;
+}
+~Course() {
 
+}
+void addStudent(const Student& s) {
+	Course prog = new Student();
 
+}
+void displayCourseInfo() {
+	cout<< courseCode <<endl;
+	cout<< courseName <<endl;
+	cout<< maxStudents <<endl;
+	cout<< student <<endl;
+	cout<< currentStudents <<endl;
 
-
-
+}
+   
 
 // ==================== Main Function ====================
 int main() {
+   Person p1* = new Student();
+	Person p2* = new Instructor();
+	Course c1* = new Course();
+   p1.display();
+   p2.display();
+   c1.displayCourseInfo();
+   c1.addStduent();
    
+
     
     return 0;
 }
