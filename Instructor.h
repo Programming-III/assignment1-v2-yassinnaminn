196a4a562f37cf6763b483a5055f659900928936
yassinnaminn <yassinahmedamin06@gmail.com>
2025-11-11 09:27:35 +0200

    completed Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,16 @@
-#ifndef INSTRUCTOR_H
-#define INSTRUCTOR_H
-
+#ifndef STUDENT_H
+#define STUDENT_H
 #include "Person.h"
-using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
-
+#include <string>
+class Student : public Person
+{
+private:
+	int yearLevel;
+	std::string major;
+public:
+	Student();
+	Student(int yearLevel, std::string major);
+	~Student();
+	void display();
+};
 #endif
